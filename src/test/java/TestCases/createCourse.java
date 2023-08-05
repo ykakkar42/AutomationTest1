07d3e09f293144c8be8925f8710269db22bc8ab7
yogesh.kakkar <yogesh.kakkar@localsystem.com>
2023-08-05 14:04:31 +0530

    new test case for course creation

---
+++ src/test/java/TestCases/createCourse.java
@@ -0,0 +1,15 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+import BaseClass.baseClass;
+
+public class createCourse extends baseClass {
+	
+	@Test
+	public void coursecreation()
+	{
+		
+	}
+
+}
