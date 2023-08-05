b3119c771079cb573f5dc31006f904c028f46890
yogesh.kakkar <yogesh.kakkar@localsystem.com>
2023-08-05 14:09:17 +0530

    test case removed

--- src/test/java/TestCases/createCourse.java
+++ src/test/java/TestCases/createCourse.java
@@ -1,15 +1,9 @@
 package TestCases;
 
-import org.testng.annotations.Test;
-
 import BaseClass.baseClass;
 
 public class createCourse extends baseClass {
 	
-	@Test
-	public void coursecreation()
-	{
-		
-	}
+
 
 }
